Added way to get incorrectly predicted samples. Optimized average_pool and applied some small improvements.

sampling/gibbs_window_expt.py
@@ -85,9 +85,11 @@ class Clamp_anything(object):
 
 
 class Clamp_window(object):
-    def __init__(self, img_shape, interval, win_size):
+    def __init__(self, img_shape, interval, win_size=None):
         self.interval = interval
         self.img_shape = img_shape
+        if win_size is None:
+            win_size = img_shape[1]
         self.win_size = win_size
 
     def __call__(self, t):
@@ -129,12 +131,13 @@ def main(general_dict):
                 rbm, duration, test_set[0][start:end], binary=binary,
                 burnin=general_dict['burn_in'], clamp_fct=clamp)
 
-            if binary:
-                samples = samples > .5
             # compared to the lif-methods, the method returns an array with
             # shape [n_steps, n_imgs, n_vis]. Hence, swap axes.
             samples = np.swapaxes(samples, 0, 1)
-            np.savez_compressed('samples', samples=samples)
+            if binary:
+                np.savez_compressed('samples', samples=samples > .5)
+            else:
+                np.savez_compressed('samples', samples=samples)
         else:
             print('Missing sample file', file=sys.stderr)
             samples = None

sampling/lif_inspect_samples.py
@@ -7,6 +7,7 @@ import matplotlib.animation as animation
 import sys
 from lif_pong.utils import tile_raster_images
 from lif_pong.utils.data_mgmt import make_figure_folder, load_images, get_rbm_dict, get_data_path
+plt.rcParams['animation.ffmpeg_path'] = u'/home/hd/hd_hd/hd_kq433/ffmpeg-3.4.1-64bit-static/ffmpeg'
 
 n_imgs = 2
 sample_file = None
@@ -68,13 +69,15 @@ if show_label:
 # nh = hid_samples.shape[-1]
 # vis_samples = rbm.sample_v_given_h(hid_samples.reshape(-1, nh))[0][:, :n_pixels]
 
-# running average over samples?
-from scipy.ndimage import convolve1d
-kwidth = 20
-kernel = np.ones(kwidth)/kwidth
-avg_samples = convolve1d(vis_samples, kernel, axis=1)
+# # running average over samples?
+# from scipy.ndimage import convolve1d
+# kwidth = 20
+# kernel = np.ones(kwidth)/kwidth
+# avg_samples = convolve1d(vis_samples, kernel, axis=1)
+avg_samples = vis_samples
 
 frames = avg_samples.reshape(-1, *img_shape)
+samples_per_frame = frames.shape[1] / (img_shape[1] + 1)
 if show_label:
     active_lab = np.argmax(lab_samples, axis=2).flatten()
 
@@ -102,6 +105,7 @@ def update_fig(i_frame):
     im.set_data(frame)
     return im, time_text, lab_text
 
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 im = ax.imshow(np.zeros(img_shape), vmin=0, vmax=1., interpolation='Nearest',
@@ -114,5 +118,5 @@ time_text = ax.text(0.05, 0.01, '', va='bottom', ha='left',
 ani = animation.FuncAnimation(fig, update_fig, frames=zip(range(len(frames)), frames),
                               interval=10., blit=True, repeat=False)
 
-# ani.save(make_figure_folder() + 'samples_U0.001_trec0.mp4')
-plt.show()
+ani.save(make_figure_folder() + 'test.mp4', writer='ffmpeg')
+# plt.show()

sampling/plot_predictions.py
@@ -89,7 +89,7 @@ def merge_chunks(basefolder, identifier_dict, savename):
           file=sys.stdout)
     if prediction is not None:
         # save prediction data (faster for re-plotting)
-        print('Saved as '+savename)
+        print('Saved as '+ savename)
         np.savez(savename, last_col=prediction, lab=prediction[..., :12],
                  data_idx=data_idx)
     else:
@@ -102,7 +102,7 @@ def get_performance_data(basefolder, identifier_dict):
     id_string = '_'.join(
         ['{}'.format(identifier_dict[k]) for k in identifier_dict.keys()])
     savename = os.path.join(make_data_folder(),
-                            basefolder.split('/')[-1] + id_string)
+                            basefolder.split('/')[-1] + '_' + id_string)
     # try to load data
     try:
         with np.load(savename + '.npz') as f:

sampling/pong_agent.py
@@ -102,11 +102,15 @@ def compute_performance(img_shape, data_name, data_idx, prediction, max_speedup=
     if max_speedup == np.inf:
         pred_error = np.abs(predicted_pos - targets.reshape((len(targets), 1)))
         successes = np.sum(pred_error < .5*paddle_width, axis=0)
+        # for inspecting failed cases
+        wrong_idx = data_idx[np.where(
+            pred_error[:, -(leave_uncovered + 1)] > .5*paddle_width)]
         result = {
             'successes': successes,
             'distances': pred_error,  # for infinite speed equivalent
             'speeds': np.inf,
-            'n_instances': len(data_idx)
+            'n_instances': len(data_idx),
+            'wrong_idx': wrong_idx
         }
         return result
 

utils/other.py
@@ -1,7 +1,7 @@
 # Some useful functions
 from __future__ import division
 import numpy as np
-import sys
+from scipy.ndimage import convolve1d
 
 
 # return a weighted average of the interval out_range for each vector in arr
@@ -13,18 +13,23 @@ def average_helper(out_range, arr):
 
 
 # used by save_prediction_data scripts
-def average_pool(arr, width, stride=None, offset=0):
+def average_pool_old(arr, width, stride=None, offset=0):
     # array needs to have shape (n_imgs, n_t, n_pxls)
     if stride is None:
         stride = width
     kernel = np.ones(width) / width
     return np.apply_along_axis(
         np.convolve, 1, arr, kernel, 'valid')[:, int(offset)::int(stride)]
-    # x = .5*(len(kernel) - 1)
-    # filtered = convolve1d(
-    #     arr, kernel, axis=1, mode='constant')[:, np.floor(x):-np.ceil(x)]
-    # print((filtered == filtered1).mean())
-    # doesn't give same result, but should be same and faster -> optimization
+
+
+def average_pool(arr, width, stride=None, offset=0):
+    # array needs to have shape (n_imgs, n_t, n_pxls)
+    if stride is None:
+        stride = width
+    kernel = np.ones(width) / width
+    x = .5*(len(kernel) - 1)
+    convol = convolve1d(arr, kernel, axis=1, mode='constant')
+    return convol[:, offset + int(np.floor(x)):-int(np.ceil(x)):int(stride)]
 
 
 # this is more a reminder how it's done than an actual function